2cbabe926a1f4921c39ce41465eb6063f279ce30
Yuto Urashima <yuto.urashima24@gmail.com>
2023-09-21 18:49:59 +0900

    変数名修正

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,18 +1,18 @@
 # ----------------------------------------
 # プライベートバケット
 # ----------------------------------------
-resource "aws_s3_bucket" "private" {
-  bucket = "private-pragmatic-terraform"
+resource "aws_s3_bucket" "private_bucket" {
+  bucket = "my-private-terraform-bucket" # バケット名を具体的なものに変更
 }
 
-resource "aws_s3_bucket_versioning" "private" {
-  bucket = aws_s3_bucket.private.id
+resource "aws_s3_bucket_versioning" "private_versioning" {
+  bucket = aws_s3_bucket.private_bucket.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "private" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "private_encryption" {
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -23,23 +23,23 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "private" {
 # ----------------------------------------
 # ブロックパブリックアクセス
 # ----------------------------------------
-resource "aws_s3_bucket_public_access_block" "private" {
-  bucket = aws_s3_bucket.private.id
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
+resource "aws_s3_bucket_public_access_block" "private_access_block" {
+  bucket                  = aws_s3_bucket.private_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
 # ----------------------------------------
 # パブリックバケット
 # ----------------------------------------
-resource "aws_s3_bucket" "public" {
-  bucket = "public-pragmatic-terraform"
-  acl = "public-read"
+resource "aws_s3_bucket" "public_website_bucket" {
+  bucket = "my-public-terraform-website-bucket" # バケット名を具体的なものに変更
+  acl    = "public-read"
 }
 
-resource "aws_s3_bucket_cors_configuration" "cors_public" {
+resource "aws_s3_bucket_cors_configuration" "public_cors" {
   cors_rule {
     allowed_origins = ["https://example.com"]
     allowed_methods = ["GET"]
